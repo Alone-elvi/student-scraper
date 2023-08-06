892c6c516d15c88da5bfa7eeda010122e07a880d
Alone-elvi <alone.elvi@gmail.com>
2023-08-06 23:23:09 +0300

    Change counting of cards

--- deskofcards/pocker.py
+++ deskofcards/pocker.py
@@ -2,17 +2,56 @@ from api.deskofcards import init_game, draw_cards
 from collections import Counter
 
 CARD_VALUE = ("2", "3", "4", "5", "6", "7", "8", "9", "0", "J", "Q", "K", "A")
+CARD_RANKS = (2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)
+
 CARD_SUIT = ("S", "H", "D", "C")
 
 
-def print_cards(cards: list) -> None:
+def print_cards(cards: list, combination=None, hand: int = 0) -> None:
+    """
+    Print the cards and the hand number.
+
+    Args:
+        cards (list): A list of cards.
+        hand (int): The hand number.
+
+    Returns:
+        None
+    """
     for id, card in enumerate(cards):
-        print(id, card["code"])
+        if card["code"][0] == "0":
+            print(id + 1, "1" + card["code"])
+        else:
+            print(id + 1, card["code"])
+
+    print("Player: ", hand + 1)
+    print("Combinations: ", combination)
+    return None
+
+
+def print_hands_ranking(move_hand: dict, hand: int = 0) -> None:
+    hands_rank = hands_ranking(move_hand["cards"])
+    if hands_rank:
+        print_cards(move_hand["cards"], hands_rank["COMBINATIONS"], hand)
+    else:
+        print_cards(move_hand["cards"], "NONE", hand)
+
+    print("Player: ", hand + 1, "Score: ", counting_score(move_hand["cards"]))
 
     return None
 
 
 def card_values(cards: list) -> list:
+    """
+    Returns a sorted list of card values.
+
+    Parameters:
+        cards (list): A list of dictionaries representing cards.
+            Each dictionary should have a 'code' key.
+
+    Returns:
+        list: A sorted list of card values.
+    """
     values = sorted(
         list(map(lambda x: x.replace("0", "10"), [x.get("code")[0] for x in cards]))
     )
@@ -20,15 +59,48 @@ def card_values(cards: list) -> list:
 
 
 def card_suites(cards: list) -> list:
+    """
+    Generate a list of card suites from a list of card dictionaries.
+
+    Args:
+        cards (list): A list of card dictionaries. Each dictionary represents
+            a card and contains a 'code' key.
+            The 'code' value is a string representing the card's code.
+
+    Returns:
+        list: A list of card suites extracted from the given list of card
+            dictionaries. The suites are sorted in ascending order.
+    """
     suites = sorted([x.get("code")[1] for x in cards])
     return suites
 
 
 def counting_same_cards(cards: list) -> Counter:
+    """
+    Create a counter object that counts the occurrences of each card
+      in the given list.
+
+    Parameters:
+        cards (list): A list of cards.
+
+    Returns:
+        Counter: A Counter object that contains the count of each card.
+    """
     return Counter(cards)
 
 
 def counting_cards(counter_values: list, result: dict) -> dict:
+    """
+    Counts the occurrences of values in a list and categorizes
+    them into pairs, threes, and fours.
+
+    Parameters:
+        counter_values (list): A list of values to be counted.
+        result (dict): A dictionary to store the categorized values.
+
+    Returns:
+        dict: A dictionary containing the categorized values.
+    """
     for i in enumerate(counter_values):
         if counter_values[i[1]] == 2:
             if "PAIR" not in result:
@@ -44,39 +116,81 @@ def counting_cards(counter_values: list, result: dict) -> dict:
     return result
 
 
+def counting_score(cards: dict) -> int:
+    """
+    Calculate the score based on the given list of cards.
+
+    Args:
+        cards (dict): A dictionary representing a list of cards.
+            Each card is a dictionary with a 'code' key that represents
+            the card code.
+
+    Returns:
+        int: The total score calculated from the cards.
+    """
+    res = sum(
+        [
+            int(CARD_RANKS[CARD_VALUE.index(card["code"][0])])
+            for card in cards
+            if card["code"][0] in CARD_VALUE
+        ]
+    )
+    return res
+
+
 def counting_result(result: dict, values: list, suites: list) -> dict:
+    """
+    Takes a dictionary `result`, a list of `values`, and a list of `suites`
+    as parameters.
+    Modifies the `result` dictionary based on certain conditions.
+    Returns the modified `result` dictionary if it is not empty, otherwise
+    returns None.
+    """
     if result:
         if "PAIR" in result:
             if len(result["PAIR"]) == 2:
                 result["TWO_PAIR"] = result["PAIR"]
                 del result["PAIR"]
                 result["TWO_PAIR"].append(True)  # Straight Flush
+                result["COMBINATIONS"] = "2 PAIR"
+            else:
+                result["COMBINATIONS"] = "PAIR"
+
+        if "THREE" in result:
+            result["COMBINATIONS"] = "THREE"
+
         if "PAIR" in result and "THREE" in result:
             del result["PAIR"]
             del result["THREE"]
             result["FULL_HOUSE"] = True  # Flush
+            result["COMBINATIONS"] = "FULL_HOUSE"
+
+        if "STRAIGHT" in result:
+            result["COMBINATIONS"] = "STRAIGHT"
+        if "FLUSH" in result:
+            result["COMBINATIONS"] = "FLUSH"
+
         if "STRAIGHT" in result and "FLUSH" in result:
             del result["STRAIGHT"]
             del result["FLUSH"]
             result["STRAIGHT_FLUSH"] = True  # Straight Flush
+            result["COMBINATIONS"] = "STRAIGHT_FLUSH"
         result["CARDS"] = [[key, val] for key, val in zip(values, suites)]
         return result
     return None
 
 
-def is_three():
-    pass
-
-
-def is_suter():
-    pass
-
-
-def is_flush():
-    pass
-
-
 def is_flush_royal(values: list, result: dict) -> dict:
+    """
+    Check if the given list of values represents a flush royal hand.
+
+    Args:
+        values (list): A list of card values.
+        result (dict): A dictionary to store the result.
+
+    Returns:
+        dict: A dictionary with the result of the check.
+    """
     straight_val = sorted(
         [CARD_VALUE.index(val) for val in values if val in CARD_VALUE]
     )
@@ -101,11 +215,22 @@ def is_flush_royal(values: list, result: dict) -> dict:
 
 
 def hands_ranking(cards):
+    """
+    Generate the ranking of hands based on the given cards.
+
+    Parameters:
+    cards (list): A list of cards representing a hand.
+
+    Returns:
+    dict: A dictionary containing the ranking of hands based on the given cards.
+          The dictionary may include the following keys:
+          - 'FLUSH': True if the hand is a flush, False otherwise.
+          - 'ROYAL_FLUSH': True if the hand is a royal flush, False otherwise.
+          - 'STRAIGHT': True if the hand is a straight, False otherwise.
+    """
     values = card_values(cards)
     suites = card_suites(cards)
 
-    # values = sorted(["J", "J", "Q", "Q", "Q"])
-    # suites = ["S", "D", "S", "S"]
     result = {}
 
     counter_values = counting_same_cards(values)
@@ -124,62 +249,27 @@ def hands_ranking(cards):
         elif "STRAIGHT" in tmp_result:
             result["STRAIGHT"] = True
 
-    # straight_val = sorted(
-    #     [CARD_VALUE.index(val) for val in values if val in CARD_VALUE]
-    # )
-
-    # straight_count = 0
-    # royal_flush = False
-
-    # for id, value in enumerate(straight_val):
-    #     if id == 0 and value == 8:
-    #         royal_flush = True
-    #     if id != 0:
-    #         for prev_value in straight_val[id - 1 : id]:
-    #             diff = value - prev_value
-    #             if diff == 1:
-    #                 straight_count += 1
-
-    # if straight_count == 4:
-    #     if royal_flush:
-    #         result["ROYAL_FLUSH"] = True
-    #         del result["FLUSH"]
-    #     else:
-    #         result["STRAIGHT"] = True
-
-    # Royal Flush
-
     cards_count = counting_cards(counter_values, result)
     result_count = counting_result(cards_count, values, suites)
 
-    print_cards(cards)
     return result_count
 
 
-if __name__ == "__main__":
-    desk_id = init_game()
+def api_get_cards(hand: int, turn: bool, desk_id: str, move_hand: dict) -> dict:
+    want_change = str()
 
-    one_hand = draw_cards(desk_id)
-    second_hand = draw_cards(desk_id)
-
-    remaining = second_hand["remaining"]
-
-    while remaining:
-        hands_rank = hands_ranking(second_hand["cards"])
-        if hands_rank:
-            print(hands_rank)
-
-        player_hand = second_hand
-
+    if not turn:
+        want_change = input("How many cards do you want to change? ")
+    elif want_change.isdigit():
+        move_hand = draw_cards(desk_id, want_change)
+    else:
+        move_hand = draw_cards(desk_id)
+        print_hands_ranking(move_hand, hand)
         want_change = input("How many cards do you want to change? ")
 
-        second_hand = draw_cards(desk_id, want_change)
+    player_hand = move_hand
 
-        [
-            print((id + 1, card["code"]), sep="\n")
-            for id, card in enumerate(player_hand["cards"])
-        ]
-
+    if want_change.isdigit():
         changeing_card = []
         for card in range(int(want_change)):
             changeing_card.append(
@@ -188,13 +278,78 @@ if __name__ == "__main__":
                 ]
             )
 
-        for card in player_hand["cards"]:
-            if card["code"] in [cange_card["code"] for cange_card in changeing_card]:
-                player_hand["cards"].remove(card)
+        for changed_card in move_hand["cards"]:
+            for id, card in enumerate(player_hand["cards"]):
+                if card["code"] in [chaned["code"] for chaned in changeing_card]:
+                    player_hand["cards"][id] = changed_card
+                    break
 
-        player_hand["cards"] += second_hand["cards"]
-        second_hand["cards"] = player_hand["cards"]
+    return player_hand
 
-        remaining = second_hand["remaining"]
+
+if __name__ == "__main__":
+    desk_id = init_game()
+
+    first_hand = {}
+    second_hand = {}
+    enough = "y"
+    move_hand = {}
+
+    hand = 0
+    player = hand
+
+    # print_hands_ranking(move_hand, hand)
+    remaining = True
+
+    while remaining:
+        if enough.lower() == "y":
+            move_hand = api_get_cards(
+                hand=hand, turn=True, desk_id=desk_id, move_hand=move_hand
+            )
+        else:
+            move_hand = api_get_cards(
+                hand=hand, turn=False, desk_id=desk_id, move_hand=move_hand
+            )
+
+        # print_hands_ranking(move_hand, hand)
+        # move_hand["cards"] = player_hand["cards"]
+        remaining = move_hand["remaining"]
+        player_hand = move_hand
+        print_hands_ranking(move_hand, hand)
+
+        # hands_rank = hands_ranking(move_hand["cards"])
+
+        # if hands_rank:
+        #     print_cards(move_hand["cards"], hands_rank["COMBINATIONS"], hand)
+        # else:
+        #     print_cards(move_hand["cards"], "NONE", hand)
+
+        move_hand["score"] = counting_score(move_hand["cards"])
+
+        enough = input("Do you want to continue? (y/n) ")
+
+        if hand == 0:
+            first_hand = move_hand
+        else:
+            second_hand = move_hand
+
+        if enough.lower() == "y":
+            hand = 1 if hand == 0 else 2
+            # player = hand
+
+        #     move_hand = api_get_cards(hand, desk_id)
+
+        # hands_rank = hands_ranking(move_hand["cards"])
+
+        # print_hands_ranking(move_hand, hand)
+
+        # if hands_rank:
+        #     print_cards(move_hand["cards"], hands_rank["COMBINATIONS"], hand)
+        # else:
+        #     print_cards(move_hand["cards"], "NONE", hand)
+
+        # print("Player: ", hand, counting_score(move_hand["cards"]))
+
+        remaining = move_hand["remaining"]
     # print(one_hand, second_hand)
 # @TODO Сделать, чтобы после смены карт, менялся участник
